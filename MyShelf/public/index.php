4c259704831edd71af57f1c902a31bb2f8a5681a
NetoSantosOP7 <148675257+NetoSantosOP7@users.noreply.github.com>
2026-01-24 03:41:49 -0300

    Update index.php

--- MyShelf/public/index.php
+++ MyShelf/public/index.php
@@ -4,21 +4,27 @@ session_start();
 
 require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../vendor/autoload.php';
+
 require_once __DIR__ . '/../app/Models/Database.php';
 require_once __DIR__ . '/../app/Models/Usuario.php';
 require_once __DIR__ . '/../app/Models/Colecao.php';
 require_once __DIR__ . '/../app/Models/Livro.php';
-require_once __DIR__ . '/../app/Helpers/PdfHelper.php';
-require_once __DIR__ . '/../app/Core/Router.php';
 require_once __DIR__ . '/../app/Models/Marcador.php';
+require_once __DIR__ . '/../app/Helpers/PdfHelper.php';
 require_once __DIR__ . '/../app/Helpers/Preferencias.php';
+require_once __DIR__ . '/../app/Core/Router.php';
 
-$requestUri = $_SERVER['REQUEST_URI'];
-$scriptName = $_SERVER['SCRIPT_NAME'];
 
-$url = str_replace(dirname($scriptName), '', $requestUri);
-$url = trim($url, '/');
-$url = strtok($url, '?');
+$requestUri = $_SERVER['REQUEST_URI'] ?? '/';
+
+$path = parse_url($requestUri, PHP_URL_PATH);
+
+$baseUrl = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME']));
+if ($baseUrl !== '/') {
+    $path = str_replace($baseUrl, '', $path);
+}
+
+$url = trim($path, '/');
 
 if (empty($url)) {
     $url = '';
